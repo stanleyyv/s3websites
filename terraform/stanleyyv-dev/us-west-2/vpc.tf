2e28cfe4b1378cafb691f02241105b7ba63ba6b0
stanleyyv <stanleyyv@gmail.com>
2022-09-01 02:26:15 -0500

    created route table for subnet 1, route association and internet gateway route for subnet 1

--- terraform/stanleyyv-dev/us-west-2/vpc.tf
+++ terraform/stanleyyv-dev/us-west-2/vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "private_vpc_1" {
 }
 
 
-resource "aws_subnet" "pubic_subnet_1" {
+resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.private_vpc_1.id
   cidr_block              = "10.0.0.0/26"
   map_public_ip_on_launch = "true"
@@ -26,4 +26,22 @@ resource "aws_internet_gateway" "vpc_1" {
   tags = {
     Name = "dev-us-west-2"
   }
+}
+
+resource "aws_route_table" "public_subnet_1" {
+  vpc_id = aws_vpc.private_vpc_1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc_1.id
+  }
+
+  tags = {
+    Name = "dev-us-west-2-public-subnet-1-rtb"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_subnet_1.id
 }
